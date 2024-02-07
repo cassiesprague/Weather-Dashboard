3674b0b91775adb4fcdc052e29388b7cac8542ac
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-02-06 19:20:26 -0500

    added function and html

--- Assets/script.js
+++ Assets/script.js
@@ -3,3 +3,34 @@ var currentDate = (now.format("MM/DD/YYYY"));
 var city = "";
 var citySearch = $("#city-search");
 var citySearchButton = $("#city-search-button");
+
+//Clears localstorage
+function deleteItems() {
+    localStorage.clear();
+}
+
+//Displays weather after clicking search button
+citySearchButton.on("click", displayWeather);
+
+//displays weather after running currentWeather function
+function displayWeather(event) {
+    event.preventDefault();
+    if (citySearch.val().trim() !== "") {
+        city = citySearch.val().trim();
+        currentWeather(city);
+
+        //localstorage for searched cities
+        var cityList = document.getElementById("city-list");
+        cityList.textContent = "";
+
+        var searchedCities = localStorage.getItem("visitedCities");
+        if (searchedCities === null) {
+            searchedCities = [];
+        } else {
+            searchedCities = JSON.parse(searchedCities);
+        }
+        searchedCities.push(city);
+
+        var visitedCityNames = JSON.stringify(searchedCities);
+        localStorage.setItem("visitedCities", visitedCityNames);
+
