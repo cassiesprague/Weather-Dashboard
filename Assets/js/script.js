63b1f1290eb24367f78738eb555f62f213d797bc
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-03-30 14:20:12 -0400

    added acceptance criteria to dos, created api key, deleted css

---
+++ Assets/js/script.js
@@ -0,0 +1,24 @@
+// Open Weather Map link: https://openweathermap.org/forecast5
+// Open Weather Map API key: daf8f38305e86af0479c8ba66ec63552
+
+
+//Below are the variables
+const APIKey: "daf8f38305e86af0479c8ba66ec63552";
+
+
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+
+
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the 
+// humidity, and the wind speed
+
+
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the 
+// temperature, the wind speed, and the humidity
+
+
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
