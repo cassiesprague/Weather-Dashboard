d23c7a6e780899563bcd68e46c46322558b9a558
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-03-31 18:20:10 -0400

    added first part of function to get locations weather of searched city

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,7 +8,16 @@ const APIKey: "daf8f38305e86af0479c8ba66ec63552";
 
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
-
+var searchCity = document.getElementsById("search-input");
+function getCityLocation(city){
+    var geoURL = "https://api.openweathermap.org/data/2.5/weather?q=" + "city" + "&appid=" + "APIKey"
+    fetch(geoURL).then(function(response){
+        return response.json()
+    }) .then(function(data){
+        console.log(data)
+        getWeather(data[0].latitude,data[0].longitude)
+    })
+}
 
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the 
