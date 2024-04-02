b6e3faba6b88e8f76b1d806368d2d04b18d8942a
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-04-02 18:46:32 -0400

    added comment of video link and event listener for button to fetch info

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,11 +3,25 @@
 
 
 //Below are the variables
+//The link below is a reference I used to help with the js for this weather dashboard
+//https://www.youtube.com/watch?v=GXrDEA3SIOQ
 const APIKey: "daf8f38305e86af0479c8ba66ec63552";
+var button = document.querySelector('#search-button')
+var input = document.querySelector('#search-input')
+var recentSearches = document.querySelector('#recent-searches')
 
 
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
+button.addEventListener('click', function(){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+ input.value+'&appid=daf8f38305e86af0479c8ba66ec63552')
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+    .catch(err => alert("Invalid City Name"))
+})
+
+
 
 
 // WHEN I view current weather conditions for that city
