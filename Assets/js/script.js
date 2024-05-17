c8b82c98b2e6a771233c988f9f9b31c66f03af33
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-05-17 16:19:21 -0400

    added finction to display the current weather for given location

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,6 +7,7 @@
 // https://www.youtube.com/watch?v=QEu8_5bYm-w&t=106s 
 // https://www.youtube.com/watch?v=y9ZBHoUTWjY&list=PLAEoBV_GLyq4klW-2Pm75_5-r4oHhwqlm&index=3 
 // https://www.youtube.com/watch?v=KT6Jaxl0JM4&list=PLAEoBV_GLyq4klW-2Pm75_5-r4oHhwqlm&index=4 
+// https://www.youtube.com/watch?v=6imiFFeDIzE&list=PLAEoBV_GLyq4klW-2Pm75_5-r4oHhwqlm&index=5 
 // https://www.youtube.com/watch?v=IfG4A8YfxsY 
 
 
@@ -18,60 +19,56 @@
 
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
-$(document).ready(function(){
-    $('#searchButton').click(function(){
-
-            var city = $('#city').val();
-
-            if(city!= ''){
-
-                $.ajax({
-
-                    url: "https://api.openweathermap.org/data/2.5/weather?q= " + city + "&units=imperial" + "&appid=daf8f38305e86af0479c8ba66ec63552",
-                    type: "GET",
-                    dataTyle: "jsonp",
-                    success: function(data){
-                        
-                    }
-                });
-
-            }else{
-                $('#error').html('Enter City');
-            }
-
-    });
 
 
-});
-
-
-
-// function searchCity() {
-//     var city = document.getElementById("cityInput").value;
-//     var searches = JSON.parse(localStorage.getItem("recentSearches")) || [];
-//     searches.push(city);
-//     localStorage.setItem("recentSearches", JSON.stringify(searches));
-// }
-
-// function displayRecentSearches() {
-//     var searches = JSON.parse(localStorage.getItem("recentSearches")) || [];
-//     console.log("Recent Searches:", searches);
-// }
-
-// displayRecentSearches();
 
 
 
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the 
 // humidity, and the wind speed
+$(document).ready(function () {
+    $('#searchButton').click(function () {
+
+        var city = $('#city').val();
+
+        if (city != '') {
+
+            $.ajax({
+
+                url: "https://api.openweathermap.org/data/2.5/weather?q= " + city + "&units=imperial" + "&appid=daf8f38305e86af0479c8ba66ec63552",
+                type: "GET",
+                dataTyle: "jsonp",
+                success: function (data) {
+                    var widget = show(data);
+
+                    $("#display").html(widget);
+
+                    $("#city").val('');
+                }
+            });
+
+        } else {
+            $('#error').html('Enter City');
+        }
+    });
+});
+
+function show(data) {
+    return "<h2><strong>Current Weather for " + data.name + "</strong></h2>" +
+        "<h3><strong>Weather</strong>: " + data.weather[0].main + "</h3>" +
+        "<h3><strong>Description</strong>: " + data.weather[0].description + "</h3>" +
+        "<h3><strong>Temperature</strong>: " + data.main.temp + "</h3>" +
+        "<h3><strong>Humidity</strong>: " + data.main.humidity + "</h3>" +
+        "<h3><strong>Wind Speed</strong>: " + data.wind.speed + "</h3>";
+}
 
 
 
 
 
 // WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the 
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the
 // temperature, the wind speed, and the humidity
 
 
