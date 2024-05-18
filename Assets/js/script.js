40271305632c2eb259d862b5bd8a8ed0ea723379
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-05-18 16:01:04 -0400

    added css folder, tried to get 5 day forecast in html and js, having trouble, try again later

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -11,22 +11,17 @@
 // https://www.youtube.com/watch?v=IfG4A8YfxsY 
 
 
-//Below are the variables
-
-
-
-
-
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
-
-
-
-
-
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the 
 // humidity, and the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the
+// temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
 $(document).ready(function () {
     $('#searchButton').click(function () {
 
@@ -62,18 +57,3 @@ function show(data) {
         "<h3><strong>Humidity</strong>: " + data.main.humidity + "%</h3>" +
         "<h3><strong>Wind Speed</strong>: " + data.wind.speed + "mph</h3>";
 }
-
-
-
-
-
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the
-// temperature, the wind speed, and the humidity
-
-
-
-
-
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
